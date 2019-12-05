d342fa2bda9ac7777b67be602861e6300f302630
Mike <michaellarrubis@gmail.com>
2019-12-05 16:56:51 +0800

    Fix JS issue

--- app/assets/javascripts/components/song-list.js
+++ app/assets/javascripts/components/song-list.js
@@ -1,10 +1,10 @@
 document.addEventListener("turbolinks:load", function() {
 
-  let tagItems = document.querySelectorAll('.js-color');
-  let colorArray = ["#ef5350", "#ec407a", "#ab47bc", "#5c6bc0", "#42a5f5", "#26a69a", "#ffa726", "#ff7043", "#8d6e63"]
-  let randomColor = Math.floor(Math.random()*16777215).toString(16);
+  var tagItems = document.querySelectorAll('.js-color');
+  var colorArray = ["#ef5350", "#ec407a", "#ab47bc", "#5c6bc0", "#42a5f5", "#26a69a", "#ffa726", "#ff7043", "#8d6e63"]
+  var randomColor = Math.floor(Math.random()*16777215).toString(16);
 
-  for( let i = 0; i < tagItems.length; i++ ) {
+  for( var i = 0; i < tagItems.length; i++ ) {
     tagItems[i].style.backgroundColor = colorArray[Math.floor(Math.random() * colorArray.length)];
     tagItems[i].style.opacity = 0.5;
   }
